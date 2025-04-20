48c738cff1d7071719d3d275f766eb9e7059b73b
Adnan Memic <adnan.adem.am@gmail.com>
2025-04-20 22:54:12 +0200

    Add Readme and change import in main

---
+++ README.md
@@ -0,0 +1,3 @@
+# Grade 
+
+This is a tiny project were you track your tests (grades) and homework. This program will show you your percentages of each test and homework.
--- main.go
+++ main.go
@@ -2,23 +2,39 @@ package main
 
 // import "fmt"
 import (
+	"flag"
 	"os"
 
 	"github.com/Adnan327/grader/grade"
 )
 
 func main() {
-
-	args := os.Args
+	var (
+		args         = os.Args
+		subjectList  grade.SubjectStore
+		subjectFlag  string
+		homeworkFlag string
+		testFlag     string
+	)
+	flag.StringVar(&subjectFlag, "-s", "", "help message for flagname")
+	flag.StringVar(&homeworkFlag, "-h", "", "help message for flagname")
+	flag.StringVar(&testFlag, "-t", "", "help message for flagname")
 
 	switch args[1] {
 	case "add":
-		grade.AddSubject(args[2])
-		grade.AddHomework(args[2], args[3], args[4], args[5])
-		grade.AddTest(args[2], args[3], args[4], args[5])
+		if subjectFlag != "" {
+			subjectList.AddSubject(subjectFlag)
+		} else if homeworkFlag != "" {
+			subjectList.AddHomework(homeworkFlag)
+		} else if testFlag != "" {
+			subjectList.AddTest(testFlag)
+		}
 
 	case "rm":
-
+		if homeworkFlag != "" {
+			subjectList.RemoveHomework(homeworkFlag)
+		} else if testFlag != "" {
+			subjectList.RemoveTest(testFlag)
+		}
 	}
-
 }
